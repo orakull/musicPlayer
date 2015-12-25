32552005ffcc7f0d4e7d739363821e753266cf4e
orakull <ruslan.olkhovka@gmail.com>
2015-12-26 05:12:46 +0700

    Initial Commit

---
+++ musicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicPlayer
+//
+//  Created by Руслан Ольховка on 26.12.15.
+//  Copyright © 2015 Руслан Ольховка. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
